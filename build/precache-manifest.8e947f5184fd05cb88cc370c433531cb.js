dffb665185fedc2a1df7d4d58071c2ad530b561e
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2019-12-16 18:07:31 +0530

    deploying build

---
+++ build/precache-manifest.8e947f5184fd05cb88cc370c433531cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276567076fd60477207544a3bca73b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9673311441db0484d407",
+    "url": "/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "fae64d15e1ab28c6d30b",
+    "url": "/static/css/main.928d36c1.chunk.css"
+  },
+  {
+    "revision": "9673311441db0484d407",
+    "url": "/static/js/2.4f8eaf72.chunk.js"
+  },
+  {
+    "revision": "e1ae7605f5fc3120c3b3a8c4a1ff88c7",
+    "url": "/static/js/2.4f8eaf72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fae64d15e1ab28c6d30b",
+    "url": "/static/js/main.28c04d8b.chunk.js"
+  },
+  {
+    "revision": "4ca0bf00a49b064ac421",
+    "url": "/static/js/runtime-main.f1271c84.js"
+  },
+  {
+    "revision": "4d995d23950757340e054372bf24e0c5",
+    "url": "/static/media/ens_logo.4d995d23.svg"
+  }
+]);
